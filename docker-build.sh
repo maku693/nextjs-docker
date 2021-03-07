0a65bb72c3f7e38371f5f3b4f65dbb88f03ca927
maku693 <me@maku693.net>
2021-03-07 17:42:02 +0900

    Add dockerfiles

---
+++ docker-build.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env sh
+set -eux
+
+names=$(cat <<"EOF"
+clean-cache
+install-production
+simple
+staged
+EOF)
+
+for name in $names; do
+  docker build -f "Dockerfile.${name}" -t "nextjs-docker:${name}" .
+done
+
+docker image ls nextjs-docker
